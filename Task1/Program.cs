d1dc0885503f9f73fbb0d8e9cda64304a14e23cf
Listratova Tatyana <listratova1@gmail.com>
2022-10-10 20:11:08 +0300

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // программа выводит все натуральные числа от 1 до n. Рекурсия
+
+            string PrintDigits (int a)
+            {
+                if (a == 1)
+                {
+                    return "1";
+                }
+                string result = a + PrintDigits(a - 1);
+                return result;
+            }
+
+            int n = 5;
+
+            Console.WriteLine(PrintDigits(n));
+
+            Console.ReadLine();
+                
+
+
+
+
+        }
+    }
+}
